93e3aeda36471b70d0c7e220efd322328cc22202
Rahul Chaudhary <rkctimes@gmail.com>
2021-02-28 18:01:49 +1100

    code change- moving result to a file

--- pypoll/main.py
+++ pypoll/main.py
@@ -4,12 +4,15 @@ import os
 # Module for reading CSV files
 import csv
 
+import sys
+
 csvpath = os.path.join('.', 'election_data.csv')
 voterid_number_list = []
 candidate_list = set()
 max_count = 0
 
 with open(csvpath) as csvfile:
+    
 
       # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -24,25 +27,24 @@ with open(csvpath) as csvfile:
 
 
 total_vote = len(voterid_number_list)
+with open('result.txt', 'w') as f:
+    print ("Election Results" ,file=f )
 
-print ("Election Results")
+    print("---------------------",file=f)
 
-print("---------------------")
+    print ("Total Votes: ",total_vote, file=f)
 
-print ("Total Votes: ",total_vote)
+    print (candidate_list, file=f)
 
-print (candidate_list)
+    total_candidates = len(candidate_list)
 
-total_candidates = len(candidate_list)
+    for x in candidate_list:
+        candidate_count = voterid_number_list.count(x)
+        percentage_votes = round(( candidate_count / total_vote ) * 100,2)
+        print (x,'votes count is :',candidate_count,percentage_votes,'%',file=f)
+        if candidate_count > max_count: 
+            max_count = candidate_count
+            winner = x
 
-for x in candidate_list:
-    candidate_count = voterid_number_list.count(x)
-    percentage_votes = round(( candidate_count / total_vote ) * 100,2)
 
-
-    print (x,'votes count is :',candidate_count,percentage_votes,'%')
-    if candidate_count > max_count: 
-        max_count = candidate_count
-        winner = x
-
-print("winner is", winner,'with maximum votes of:', max_count)
+    print("winner is", winner,'with maximum votes of:', max_count, file=f)
